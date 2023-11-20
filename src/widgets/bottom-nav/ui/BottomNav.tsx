13199f5a6592672f0bc667725764f229c5a7348d
Rick96b <olegkorobeinikov21@gmail.com>
2023-11-20 20:20:42 +0500

    add material ui

--- src/widgets/bottom-nav/ui/BottomNav.tsx
+++ src/widgets/bottom-nav/ui/BottomNav.tsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import {AiOutlineHome, AiOutlineCalendar} from 'react-icons/ai'
+import React, { useState } from 'react'
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 import styles from './BottomNav.module.scss'
 
 const BottomNav = () => {
+  const [value, setValue] = useState(0);
+
   return (
-    <nav className = {styles.bottomNav}>
-        <ul className={styles.navList}>
-            <li className={styles.navItem}>
-                <AiOutlineHome className={styles.navIcon}/>
-            </li>
-            <li className={styles.navItem}>
-                <AiOutlineCalendar className={styles.navIcon}/>
-            </li>
-        </ul>
-    </nav>
+    <BottomNavigation
+      showLabels={false}
+      value={value}
+      className={styles.bottomNav}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+    >
+      <BottomNavigationAction className={styles.bottomNavItem} label="Home" icon={<HomeIcon />} />
+      <BottomNavigationAction className={styles.bottomNavItem} label="Calendar" icon={<CalendarMonthIcon />} />
+    </BottomNavigation>
   )
 }
 
